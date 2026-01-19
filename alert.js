ae6b978ce3551ccf029adb2a35e13dae8334e966
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-01-19 18:48:18 +0300

    Add files via upload

--- alert.js
+++ alert.js
@@ -1,8 +1,30 @@
 'use strict';
            
-//hasLicence - имеет права
-//age - возраст
-//isDrunk - пьян
-const hasLicence=true, age=18,isDrunk=false;
 
-const result=((age>=18)&&hasLicence&&(!isDrunk))?console.log('Может'):console.log('Не может');
+let passw='клаустрофобия'; //задуманное слово
+
+function crypt(word) 
+{
+const arr=word.split('');  //Разбивка на буквы
+//    console.log(arr);  //вывожу массив - просто посмотреть, что получилось
+    for (let i = arr.length - 1; i >0; ) 
+   {   //Начиная с последней соседние буквы меняются местами.
+      let j = i - 1;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i=i-2
+   }
+    return arr.join('');
+}
+
+function check(passw,modpassw)
+{
+let res=(crypt(modpassw)===passw); //результат сравнения записываем в res
+return console.log(res);
+}
+
+let modpassw=crypt(passw);
+console.log('После перестановки получилось:',modpassw);  //вывожу результат перестановки
+console.log('После повторной перестановки получилось:',crypt(modpassw));  //вывожу результат обратной перестановки
+
+check(modpassw,'клаустрофобия');//проверка работы с правильным словом
+check(modpassw,'абракадабра');//проверка работы с неверным словом
