15bfb34e05a2b2974e90075d6797a4ae818155c1
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-02-07 20:46:45 +0300

    Update alert.js

--- 10-array-clean/alert.js
+++ 10-array-clean/alert.js
@@ -5,15 +5,16 @@ let arr2=[1,3,8,12,5,7,2,19,9];
 
 const Fillter2= (a)=> { 
                 const res2=(a) => {    
-                        if ((a%2)===0) return true;
+                        if ((a%2)===0) return true;  //проверяет на четность.
                         else  return false;} 
      
 for (let i=0;i<a.length;i++)
     {
-      if (res2(a[i])===true)  delete a[i];
+      if (res2(a[i])===true)  delete a[i];   //удаляем четные
         
     }
-return  a.flat();
+return  a.flat();  //удаляет пустые ячейки
 }
 
 console.log(Fillter2(arr2));
+
