8adbc9f257badb70b641056e5b949b327a3404c2
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-02-07 20:42:28 +0300

    Update alert.js

--- 10-array-clean/alert.js
+++ 10-array-clean/alert.js
@@ -1,24 +1,7 @@
 'use strict';
            
-let arr=[1,3,8,12,5,7,2,19,9];
+
 let arr2=[1,3,8,12,5,7,2,19,9];
-
-const res=(a) => {    //четная-нечетная
-      if ((a%2)===0) return true;
-      else return false; }
-
-const Fillter= (a)=> {
-for (let i=0;i<a.length;i++)
-    {
-      if (res(a[i])===true)  delete a[i];
-        
-    }
-return  a.flat();
-}
-
-console.log(Fillter(arr));
-
-//console.log(res(4));
 
 const Fillter2= (a)=> { 
                 const res2=(a) => {    
@@ -32,4 +15,5 @@ for (let i=0;i<a.length;i++)
     }
 return  a.flat();
 }
+
 console.log(Fillter2(arr2));
